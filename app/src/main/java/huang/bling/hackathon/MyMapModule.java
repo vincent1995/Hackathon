7aad54fb9d6548928e5f0d3e7d1349f42474964c
huangbinglin <huangbinglin01@gmail.com>
2018-10-19 17:15:11 -0500

    blaine commit

--- app/src/main/java/huang/bling/hackathon/MyMapModule.java
+++ app/src/main/java/huang/bling/hackathon/MyMapModule.java
@@ -1,4 +1,30 @@
 package huang.bling.hackathon;
 
-public class MyMapModule {
+import android.Manifest;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationManager;
+
+public class MyMapModule implements AppContract.MapModule<Integer,Location>{
+    Context context;
+    MyMapModule(Context context){
+        this.context = context;
+    }
+    @Override
+    public Integer getMap() {
+        return null;
+    }
+
+    @Override
+    public Location getCurrentLocation() {
+        if( context.checkSelfPermission(Manifest.permission.ACCESS_FINE_LOCATION)
+            == PackageManager.PERMISSION_GRANTED){
+            LocationManager locationManager = (LocationManager) context.getSystemService(Context.LOCATION_SERVICE);
+            return locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+        }
+        else{
+            return null;
+        }
+    }
 }
